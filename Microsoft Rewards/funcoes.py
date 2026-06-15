eacbd82b5083038d9dd4b9ec8560d358689709ad
moises99 <moisesp.silva@outlook.com.br>
2026-06-15 16:24:24 -0300

    Versao 2.0

--- Microsoft Rewards/funcoes.py
+++ Microsoft Rewards/funcoes.py
@@ -54,7 +54,6 @@ def func_principal():
             print()
         driver.get(f'https://www.bing.com/search?q={noticias_func[1][pos]}&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq={noticias_func[1][pos]}&sc=15-7&sk=&cvid={CHAVE_ID["chave"]}')
         load(0.05,f'Manchete {pos+1}: {titulo} ')
-        
         #Bloco responsável por verificar se o limite de pontos na posição 30 foram atingido e na posição 50 ou maior foram atingido, se não, verificará a cada pesquisa.
         if pos == 30 or pos >= 50:
             with Progress() as progresso:
@@ -74,14 +73,14 @@ def func_principal():
                 print()
                 print(texto_personalizado(f' Necessários mais {pontos_faltando} pontos').upper())
             
-        #Miau
-        if pontos_faltando == 3:
-            VARIAVEIS["controle_pontos"] +=1
-            print(f'Tentativa: {VARIAVEIS["controle_pontos"]}')
-            if VARIAVEIS["controle_pontos"] == 5:
-                print(texto_personalizado(f' Você atingiu o máximo de pontos ').upper().center(120))
-                print(resumo(nome_nivel_membro,pontos_nivel_membro,pontos_atuais,pontos_atuallizados,pos))
-                break
+            #Miau
+            if pontos_faltando == 3:
+                VARIAVEIS["controle_pontos"] +=1
+                print(f'Tentativa: {VARIAVEIS["controle_pontos"]}')
+                if VARIAVEIS["controle_pontos"] == 5:
+                    print(texto_personalizado(f' Você atingiu o máximo de pontos ').upper().center(120))
+                    print(resumo(nome_nivel_membro,pontos_nivel_membro,pontos_atuais,pontos_atuallizados,pos))
+                    break
 
 
     print(texto_personalizado(f'Rotina finalizada').upper())
